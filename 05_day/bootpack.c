8666aa0da8f6820a53a3cb32533b8ed951f955bf
knzheng <knzheng@student.ubc.ca>
2023-05-03 16:01:55 -0700

    split header files

--- 05_day/bootpack.c
+++ 05_day/bootpack.c
@@ -3,13 +3,6 @@
 #include "io.h"
 #include "pm.h"
 
-void io_hlt(void);
-void io_cli(void);
-void io_sti(void);
-void io_out8(int port, int data);
-int io_load_eflags(void);
-void io_store_eflags(int eflags);
-
 void init_palette(void);
 void set_palette(int start, int end, unsigned char *rgb);
 void box_fill8(unsigned char *vram, int xsize, unsigned char c, int x0, int y0,
