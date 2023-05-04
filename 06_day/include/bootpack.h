f4747a132be7ded4bbe86fe823408b48bbfcdaac
knzheng <knzheng@student.ubc.ca>
2023-05-04 15:19:20 -0700

    some cmt for GDT

--- 06_day/include/bootpack.h
+++ 06_day/include/bootpack.h
@@ -3,14 +3,13 @@
 
 #define ADR_BOOTINFO 0x00000ff0
 
-struct BootInfo{
-  char  cyls;
-  char  leds;
-  char  vmode;
-  char  reserve;
-  short scrnx;
-  short scrny;
-  unsigned char *vram;
+struct BootInfo{  /* 0x0ff0-0x0fff */
+  char cyls; /* 启动区读硬盘读到何处为止 */
+  char leds; /* 启动时键盘LED的状态 */
+  char vmode; /* 显卡模式为多少位彩色 */
+  char reserve;
+  short scrnx, scrny; /* 画面分辨率 */
+  char *vram;
 };
 
 #endif // _BOOTPACK_H_
--- 06_day/include/desctbl.h
+++ 06_day/include/desctbl.h
@@ -13,6 +13,15 @@
 #define AR_CODE32_ER 0x409a
 #define AR_INTGATE32 0x008e
 
+// GDT ar mode
+// 00000000（0x00）：未使用的记录表（descriptor table）。
+// 10010010（0x92）：系统专用，可读写的段。不可执行。
+// 10011010（0x9a）：系统专用，可执行的段。可读不可写。
+// 11110010（0xf2）：应用程序用，可读写的段。不可执行。
+// 11111010（0xfa）：应用程序用，可执行的段。可读不可写。
+// CPU到底是处于系统模式还是应用模式，取决于执行中的应用程序是位于访问权为
+// 0x9a的段，还是位于访问权为0xfa的段。
+
 // GDT (global(segment) descriptor table)
 struct SegmentDescriptor {
   short limit_low, base_low;
