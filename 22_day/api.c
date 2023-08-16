4b373c86551b1f31e9d27622a30d65de4232897f
knzheng <knzheng@student.ubc.ca>
2023-08-16 12:26:46 +0800

    window putstr API

--- 22_day/api.c
+++ 22_day/api.c
@@ -22,9 +22,9 @@ int *hrb_api(int edi, int esi, int ebp, int esp, int ebx, int edx, int ecx, int 
   if (edx == 1) {
     cons_putchar(cons, eax & 0xff, 1);
   } else if (edx == 2) {
-    cons_putstr(cons, (char *)ebx + ds_base);
+    cons_putstr(cons, (char *) ebx + ds_base);
   } else if (edx == 3) {
-    cons_putnstr(cons, (char *)ebx + ds_base, ecx);
+    cons_putnstr(cons, (char *) ebx + ds_base, ecx);
   } else if (edx == 4) {
     return &(task->tss.esp0);
   } else if (edx == 5) {
@@ -36,7 +36,15 @@ int *hrb_api(int edi, int esi, int ebp, int esp, int ebx, int edx, int ecx, int 
     sheet_slide(sht, 100, 50);
     sheet_updown(sht, 3); /*背景层高度3位于task_a之上*/
     reg[7] = (int)sht;
-  } 
+  } else if (edx == 6) {
+    sht = (struct Sheet *) ebx;
+    put_fonts8_asc(sht->buf, sht->bxsize, esi, edi, eax, (char *) (ebp + ds_base));
+    sheet_refresh(sht, esi, edi, esi + ecx * 8, edi + 16);
+  } else if (edx == 7) {
+    sht = (struct Sheet *) ebx;
+    box_fill8(sht->buf, sht->bxsize, ebp, eax, ecx, esi, edi);
+    sheet_refresh(sht, eax, ecx, esi + 1, edi + 1);
+  }
 
   return 0;
 }
--- 22_day/app/api.asm
+++ 22_day/app/api.asm
@@ -37,40 +37,40 @@ api_open_win:            ; int api_open_win(char *buf, int xsize, int ysize, int
   POP     EDI
   RET
 
-; api_putstr_win:         ; void api_putstr_win(int win, int x, int y, int col, int len, char *str);
-;   PUSH    EDI
-;   PUSH    ESI
-;   PUSH    EBP
-;   PUSH    EBX
-;   MOV     EDX, 6
-;   MOV     EBX, [ESP+20] ; win
-;   MOV     ESI, [ESP+24] ; x
-;   MOV     EDI, [ESP+28] ; y
-;   MOV     EAX, [ESP+32] ; col
-;   MOV     ECX, [ESP+36] ; len
-;   MOV     EBP, [ESP+40] ; str
-;   INT     0x40
-;   POP     EBX
-;   POP     EBP
-;   POP     ESI
-;   POP     EDI
-;   RET
+api_putstr_win:         ; void api_putstr_win(int win, int x, int y, int col, int len, char *str);
+  PUSH    EDI
+  PUSH    ESI
+  PUSH    EBP
+  PUSH    EBX
+  MOV     EDX, 6
+  MOV     EBX, [ESP+20] ; win
+  MOV     ESI, [ESP+24] ; x
+  MOV     EDI, [ESP+28] ; y
+  MOV     EAX, [ESP+32] ; col
+  MOV     ECX, [ESP+36] ; len
+  MOV     EBP, [ESP+40] ; str
+  INT     0x40
+  POP     EBX
+  POP     EBP
+  POP     ESI
+  POP     EDI
+  RET
 
-; api_boxfill_win:        ; void api_boxfill_win(int win, int x0, int y0, int x1, int y1, int col);
-;   PUSH    EDI
-;   PUSH    ESI
-;   PUSH    EBP
-;   PUSH    EBX
-;   MOV     EDX, 7
-;   MOV     EBX, [ESP+20] ; win
-;   MOV     EAX, [ESP+24] ; x0
-;   MOV     ECX, [ESP+28] ; y0
-;   MOV     ESI, [ESP+32] ; x1
-;   MOV     EDI, [ESP+36] ; y1
-;   MOV     EBP, [ESP+40] ; col
-;   INT     0x40
-;   POP     EBX
-;   POP     EBP
-;   POP     ESI
-;   POP     EDI
-;   RET
+api_boxfill_win:        ; void api_boxfill_win(int win, int x0, int y0, int x1, int y1, int col);
+  PUSH    EDI
+  PUSH    ESI
+  PUSH    EBP
+  PUSH    EBX
+  MOV     EDX, 7
+  MOV     EBX, [ESP+20] ; win
+  MOV     EAX, [ESP+24] ; x0
+  MOV     ECX, [ESP+28] ; y0
+  MOV     ESI, [ESP+32] ; x1
+  MOV     EDI, [ESP+36] ; y1
+  MOV     EBP, [ESP+40] ; col
+  INT     0x40
+  POP     EBX
+  POP     EBP
+  POP     ESI
+  POP     EDI
+  RET
