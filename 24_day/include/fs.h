2f5977e2716c9891fd466fbe4c985f61cb9aa752
knzheng <knzheng@student.ubc.ca>
2023-08-23 15:32:48 +0800

    upload day 24

---
+++ 24_day/include/fs.h
@@ -0,0 +1,17 @@
+#ifndef _FS_H_
+#define _FS_H_
+
+#define ADR_DISKIMG 0x00100000
+
+struct FileInfo {
+  unsigned char name[8], ext[3], type;
+  char reserve[10];
+  unsigned short time, date, clustno;
+  unsigned int size;
+};
+
+void file_read_fat(int *fat, unsigned char *img);
+void file_load_file(int clustno, int size, char *buf, int *fat, char *img);
+struct FileInfo *file_search(char *name, struct FileInfo *finfo, int max);
+
+#endif // _FS_H_
