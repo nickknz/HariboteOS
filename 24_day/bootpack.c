ba222592ae46e7c2e57ecedb7608ce594e85d0c5
knzheng <knzheng@student.ubc.ca>
2023-08-24 23:25:02 +0800

    switch window using mouse

--- 24_day/bootpack.c
+++ 24_day/bootpack.c
@@ -305,6 +305,11 @@ int main(void) {
                 if (0 <= x && x < sht->bxsize && 0 <= y && y < sht->bxsize) {
                   if (sht->buf[y * sht->bxsize + x] != sht->col_inv) {
                     sheet_updown(sht, shtctl->top - 1);
+                    if (sht != key_win) {
+                      cursor_c = keywin_off(key_win, sht_win, cursor_c, cursor_x);
+                      key_win = sht;
+                      cursor_c = keywin_on(key_win, sht_win, cursor_c);
+                    }
                     if (3 <= x && x < sht->bxsize - 3 && 3 <= y && y < 21) {
                       mmx = mx;   /*进入窗口移动模式*/
                       mmy = my;
